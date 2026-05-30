88b80d5c354f9ce119d53e4eb5a60040cc5c1fdd
Gavin Astur <gavin.astur@fuori.co.uk>
2026-05-30 09:19:16 +0100

    chore: split out input handling

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,15 @@
-import { run } from './index.js';
+import { getInstructions, run } from './index.js';
 
 describe('martian robots tests', () => {
-  test('run should process all robots', () => {
-    expect(run('5 3\n1 1 E\nRFRFRFRF\n3 2 N\nFRRFLLFFRRFLL\n0 3 W\nLLFFFLFLFL')).toStrictEqual('1 1 E\n3 3 N LOST\n2 3 S');
+  describe('getInstructions', () => {
+    test('getInstructions should throw when input missing', () => {
+      expect(() => getInstructions('')).toThrow('Missing instructions, see README for examples.');
+    });
+  });
+
+  describe.skip('run', () => {
+    test('run should process all robots', () => {
+      expect(run('5 3\n1 1 E\nRFRFRFRF\n3 2 N\nFRRFLLFFRRFLL\n0 3 W\nLLFFFLFLFL')).toStrictEqual('1 1 E\n3 3 N LOST\n2 3 S');
+    });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
+export const getInstructions = (input: string) => {
+  console.log('getInstructions:input:', input);
+  if (!input) {
+    throw new Error('Missing instructions, see README for examples.');
+  }
+  return {};
+};
+
 export const run = (input: string) => {
-  console.log('input:', input);
+  console.log('run:input:', input);
+  const instructions = getInstructions(input);
+  console.log('run:instructions:', instructions);
+  //TODO deal with instructions
   return '';
 };

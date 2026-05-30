faedf5b49ec1919b941e065dc8899c76ccf48e66
Gavin Astur <gavin.astur@fuori.co.uk>
2026-05-30 08:35:15 +0100

    chore: init repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# Martian Robots
+
+A program that determines each sequence of robot positions and reports the final position of the robot.
---
+++ vitest.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    /* for example, use global to avoid globals imports (describe, test, expect): */
+    globals: true,
+    environment: 'node',
+    include: ['src/**/*.test.ts'],
+    coverage: {
+      provider: 'v8',
+      include: ['src/**'],
+      exclude: ['src/main/types/**', 'src/test/**', '*.json'],
+      reporter: ['html', 'text', 'json-summary', 'json', 'clover'],
+      thresholds: {
+        lines: 80,
+        branches: 80,
+        functions: 70,
+        statements: 80,
+      },
+    },
+  },
+});
